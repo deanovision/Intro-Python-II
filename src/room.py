b9c923c3f5976e25c81b9579a340e5c2901ca43d
deanovision <rodeanfraser@gmail.com>
2019-07-11 21:20:26 -0400

    updated room model

--- src/room.py
+++ src/room.py
@@ -1,10 +1,9 @@
-from item import Item
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
 
 class Room:
-    def __init__(self, name, description, item_list):
+    def __init__(self, name, description, *item_list):
         self.name = name
         self.description = description
         self.n_to = None
@@ -12,9 +11,32 @@ class Room:
         self.e_to = None
         self.w_to = None
         self.list = []
-        for item in item_list:
-            self.list.append(Item(item["name"], item["description"]))
+        if len(item_list) > 0:
+            for item in item_list:
+                self.list.append(item)
 
-    # def add_items_to_room(self, item_list):
-    #     for item in item_list:
-    #         self.list.append(Item(item["name"], item["description"]))
+    def add_item(self, item):
+        self.list.append(item)
+
+    def remove_item(self, item):
+        self.list.remove(item)
+
+    def locate_item(self, item_name):
+        for item in self.list:
+            if item.name.lower() == item_name.lower():
+                return item
+            else:
+                return None
+
+    def print_items(self):
+        if self.list == []:
+            print("No items available in this room")
+        else:
+            for item in self.list:
+                print(f"Items available: {item.name}")
+                # list_of_items_by_name = []
+                # list_of_items_by_name.append(item.name)
+                # print(f"Items available: {list_of_items_by_name}")
+
+    def __str__(self):
+        return f"{self.name} \n {self.description} \n"
